47629e248e1aae0a295f5ad50f644260d865a2e7
justcuongg <manhcuongn2004@gmail.com>
2024-01-26 20:26:26 +0700

    Giai phuong trinh bac nhat

---
+++ baitapthuchanh/src/phuongtrinhbac1.java
@@ -0,0 +1,26 @@
+package baitapthuchanh.src;
+
+import java.util.Scanner;
+
+public class phuongtrinhbac1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double hsA, hsB, nghiem;
+        System.out.print("Nhap he so a: ");
+        hsA = sc.nextDouble();
+        System.out.print("Nhap he so b: ");
+        hsB = sc.nextDouble();
+        System.out.println("Phuong trinh co dang la " + hsA + "x + " + hsB + " = 0");
+
+        if(hsA == 0) {
+            if(hsB == 0) System.out.println("Phuong trinh co dang 0 = 0 (luon dung)");
+            else System.out.println("Phuong trinh khong ton tai do a phai khac 0 ");
+            }
+            else
+            {
+                nghiem = -hsB / hsA;
+                System.out.println("Phuong trinh co nghiem la x = " + nghiem);
+            }
+        sc.close();
+    }
+}
