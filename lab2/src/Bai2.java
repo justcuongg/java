f6da97c75cdbdf14f9214ed20d0c9a6ebe66b8d7
justcuongg <manhcuongn2004@gmail.com>
2024-01-26 20:22:26 +0700

    Bai 2 lab 2

---
+++ lab2/src/Bai2.java
@@ -0,0 +1,19 @@
+package lab2.src;
+
+import java.util.Scanner;
+
+public class Bai2 {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+
+        System.out.print("Nhap so: ");
+        n = sc.nextInt();
+
+        if(n % 2 == 0) System.out.println("So ban nhap la so chan");
+        else System.out.println("So ban nhap la so le");
+        sc.close();
+    }
+}
