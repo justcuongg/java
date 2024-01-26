2481baaa0eb5173064865e3795e15d988b4b0b36
justcuongg <manhcuongn2004@gmail.com>
2024-01-26 21:00:24 +0700

    Bai 3 lab 2

---
+++ lab2/src/Bai3.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Bai3 {
+     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int age;
+        String name;
+
+        System.out.print("Nhap ten: ");
+        name = sc.nextLine();
+        System.out.print("Nhap tuoi: ");
+        age = sc.nextInt();
+        
+        if(age < 16)
+            System.out.println("Ban " + name + " o do tuoi vi thanh nien");
+        else if(age >= 16 && age < 18)
+            System.out.println("Ban " + name + " o do tuoi truong thanh");
+        else 
+            System.out.println("Ban " + name + " da gia");
+
+        sc.close();
+    }
+}
