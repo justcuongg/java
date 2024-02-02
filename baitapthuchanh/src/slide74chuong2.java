35711e02e61695c75468186891faa2f7415f8a10
justcuongg <manhcuongn2004@gmail.com>
2024-02-02 21:12:05 +0700

    Slide 81 chuong 2

--- baitapthuchanh/src/slide74chuong2.java
+++ baitapthuchanh/src/slide74chuong2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class slide74 {
+public class slide74chuong2 {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
