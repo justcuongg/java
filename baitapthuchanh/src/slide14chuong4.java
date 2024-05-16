0ad216e7fb60d51dc6fbea7eca114588834c9e0f
justcuongg <manhcuongn2004@gmail.com>
2024-05-16 22:33:02 +0700

    Slide 14 chuong 4

---
+++ baitapthuchanh/src/slide14chuong4.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class slide14chuong4 {
+    public static void main(String[] args) {
+        
+        ArrayList<String> arrListString1 = new ArrayList<>();
+        ArrayList<String> arrListString2 = new ArrayList<>(10);
+    }
+}
